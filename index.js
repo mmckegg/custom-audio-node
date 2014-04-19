ec8bf0f9529e9c783466e132323cd5b8ec784305
Matt McKegg <matt@wetsand.co.nz>
2014-04-19 16:54:09 +1200

    callback on connect/disconnect

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var createAudioParam = require('./audio-param')
 
-module.exports = function(input, output, params){
+module.exports = function(input, output, params, onDestinationChange){
   var audioContext = (input || output).context
 
   var node = audioContext.createGain()
+  node._onDestinationChange = onDestinationChange
 
   if (input){
     node.connect(input)
   }
 
   node._output = output
+  node._targetCount = 0
 
   if (output){
     node.connect = connect
@@ -24,11 +26,19 @@ module.exports = function(input, output, params){
 module.exports.createAudioParam = createAudioParam
 
 function connect(destination, channel){
+  this._targetCount += 1
   this._output.connect(destination, channel)
+  if (typeof this._onDestinationChange === 'function'){
+    this._onDestinationChange(this._targetCount)
+  }
 }
 
 function disconnect(param){
+  this._targetCount = 0
   this._output.disconnect(param)
+  if (typeof this._onDestinationChange === 'function'){
+    this._onDestinationChange(this._targetCount)
+  }
 }
 
 function addAudioParams(node, params){
